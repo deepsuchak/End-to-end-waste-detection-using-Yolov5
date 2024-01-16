782a952a1569e93b91c660a83e20d9d5e2c6ce10
Deep Suchak <you@example.com>
2024-01-16 15:07:04 -0500

    requirements and venv done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wastedetection",
+    version="0.0.0",
+    author="Deep Suchak",
+    author_email="suchakdeep@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
+
+## to define how your package should be distributed. Mainly when you want others to be able to install your package.
